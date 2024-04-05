d15224c1bd04e2a148f180517df7113af84625e5
Biruk Desta <150334999+Broock00@users.noreply.github.com>
2024-04-04 23:50:51 -0700

    Find Runner up score

--- runner_up_score.py
+++ runner_up_score.py
@@ -1,8 +1,8 @@
-def sec_max():
+def runner_up_score():
     lst = set(arr)
     lst.remove(max(lst))
     return max(lst)
 if __name__ == '__main__':
     n = int(input())
     arr = map(int, input().split())
-    print(sec_max())
+    print(runner_up_score())
