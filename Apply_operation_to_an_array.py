a8b8f276ec314a639071553a128b21cb8b0c5ee7
Biruk Desta <150334999+Broock00@users.noreply.github.com>
2024-04-08 11:49:52 -0700

    Apply operation to an array

---
+++ Apply_operation_to_an_array.py
@@ -0,0 +1,25 @@
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        i = 0
+        while i < len(nums) - 1:
+            if nums[i] == nums[i + 1]:
+                nums[i] = 2 * nums[i]
+                nums[i+1] = 0
+                i += 1
+            else:
+                i += 1
+                
+        j = 0
+        k = j + 1
+        while j < len(nums) and k < len(nums):
+            if nums[j] == 0:
+                if nums[k] != 0:
+                    nums[j], nums[k] = nums[k], nums[j]
+                    j += 1
+                    k += 1
+                else:
+                    k += 1
+            else:
+                j += 1
+                k += 1
+        return nums
