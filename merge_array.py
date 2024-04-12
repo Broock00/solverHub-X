46b340c85b8cdf0b2bd8f284647d0ec0278e894c
Biruk Desta <150334999+Broock00@users.noreply.github.com>
2024-04-12 13:28:54 +0300

    Merge arrays using two pinter

---
+++ merge_array.py
@@ -0,0 +1,17 @@
+n = input().split()
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+i = 0
+j = 0
+c = []
+while i < len(a) and j < len(b):
+    if a[i] < b[j]:
+        c.append(a[i])
+        i += 1
+    else:
+        c.append(b[j])
+        j += 1
+c += a[i:]
+c += b[j:]
+result = ' '.join(map(str, c))
+print(result)
